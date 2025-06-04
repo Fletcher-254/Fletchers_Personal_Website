a344adfb399c4435145d56cd193669da6a4196b8
Fletcher-254 <fletcher.nyawira@student.moringaschool.com>
2025-06-04 10:02:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,2 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("nav ul li a");
-  
-    window.addEventListener("scroll", () => {
-      let current = "";
-      sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        if (pageYOffset >= sectionTop - 80) {
-          current = section.getAttribute("id");
-        }
-      });
-  
-      navLinks.forEach((link) => {
-        link.classList.remove("active");
-        if (link.getAttribute("href") === `#${current}`) {
-          link.classList.add("active");
-        }
-      });
-    });
-  });
-  
+   
+    
